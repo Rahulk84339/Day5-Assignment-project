cac369c08adc7bee48676f9b9f6aabd14884b38e
Rahul Sharma <rahul.sharma20030158@gmail.com>
2021-09-27 13:00:56 +0530

    modify header, added hamburger menu

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 window.onscroll = function () {
   if (window.pageYOffset > 10) {
-    document.getElementById("header").style.backgroundColor = "#9B3D3D";
+    document.getElementById("header").style.backgroundColor = "#9b3d3d";
   } else {
     document.getElementById("header").style.backgroundColor = "transparent";
   }
 };
+
+var isNavVisible = false;
+
+function shownav() {
+  if (isNavVisible === false) {
+    document.getElementById("nav").style.height = "290px";
+    isNavVisible = true;
+  } else {
+    document.getElementById("nav").style.height = "0px";
+    isNavVisible = false;
+  }
+}
